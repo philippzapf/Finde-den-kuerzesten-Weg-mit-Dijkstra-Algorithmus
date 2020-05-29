b1fff64e40225e9b1423b92f6cff28df3dfa80e3
philippzapf <p-zapf@web.de>
2020-05-29 17:22:04 +0200

    first commit

---
+++ src/Pathfinder/Pathfinder.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+import './Pathfinder.css';
+
+export default class Pathfinder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 15; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
+    }
+
+    render() {
+        const {nodes} = this.state;
+        console.log(nodes);
+
+        return (
+            <div className="grid">
+                {nodes.map((row, rowIdx) => {
+                    return <div>
+                        {row.map((node, nodeIdx) => <Node></Node>)}
+                    </div>
+                })}
+            </div>
+        );
+        
+    }
+}
